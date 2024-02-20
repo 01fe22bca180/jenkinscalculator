508bc26acb61a53114fa36c762787c607ceb94d2
abhishek <01fe22bca180@kletech.ac.in>
2024-02-20 09:52:41 +0530

    sqaure and cube

---
+++ extend.java
@@ -0,0 +1,15 @@
+/**
+ * The Child class contains methods for calculating the square, cube, and modulo of a given number.
+ */import java.util.Scanner;
+ 
+ public class extend{
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+    float Modulo(float operand_1 , float operand_2){
+        return(operand_1 % operand_2);
+    }
+}
